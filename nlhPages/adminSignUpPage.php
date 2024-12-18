c8f36f19990aa5f6a552aa599f887356df4326fa
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2024-12-18 14:33:37 +0000

    Update adminSignUpPage.php
    - amended page title

--- nlhPages/adminSignUpPage.php
+++ nlhPages/adminSignUpPage.php
@@ -57,7 +57,7 @@ if (isset($_POST["submit"])) {
 <meta charset="UTF-8">
 
 <head>
-    <title>User Sign Up - Nine Lives Haven</title>
+    <title>Administrator Sign Up - Nine Lives Haven</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
