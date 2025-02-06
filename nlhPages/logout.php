717536875419438fc6ce5c82cc5b4f3049bbf666
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2025-02-06 11:49:58 +0000

    Update logout.php
    updated js

--- nlhPages/logout.php
+++ nlhPages/logout.php
@@ -2,6 +2,33 @@
 session_start();
 session_unset();
 session_destroy();
-echo '<script>alert("You have been logged out.");</script>';
-echo '<script>window.location.href = "homePage.php";</script>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logging Out - Nine Lives Haven</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+    <script>
+        Swal.fire({
+            icon: "success",
+            title: "You have been logged out.",
+            confirmButtonColor: "#f4ac6d"
+        }).then(function() {
+            window.location.href = "homePage.php";
+        });
+    </script>
+</body>
+
+</html>
+
+<?php
 exit;
+?>
