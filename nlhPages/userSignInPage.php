44340a3e5dd3c0d28954201f823ca9362daa1b6b
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2025-02-06 11:45:34 +0000

    Update userSignInPage.php
    updated for header, footer and css

--- nlhPages/userSignInPage.php
+++ nlhPages/userSignInPage.php
@@ -2,191 +2,55 @@
 <html lang="en">
 <meta charset="UTF-8">
 
-<?php
-session_start();
-
-require 'connectdb.php';
-
-// if the user is logged in, they are redirected
-if(isset($_SESSION['user_id'])){
-    header('Location:homePage.php');
-    } else { ?>
-
 <head>
     <title>Sign In - Nine Lives Haven</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
-
+    <link rel="stylesheet" href="../nlhCSS/userSignInCSS.css">
 </head>
 
-<body>
-    <header>
-        <nav class="banner">
-            <a href="homePage.php"><img src="logo" class="logo" alt="Company Logo"></a>
-            <!-- navigation bar -written using php to show 1 of three versions depending on 
-                    the type of user i.e. Admin, Registered User or Visitor-->
-            <?php
-            if (isset($_SESSION['loggedin'])) {
-                if (isset($_SESSION['admin_status']) && $_SESSION['admin_status'] == 1) {
+<?php
+session_start();
+
+require 'connectdb.php';
+include 'navbar.php'; // banner and nav bar
+
+// if the user is logged in, they are redirected
+if (isset($_SESSION['user_id'])) {
+    header('Location:homePage.php');
+} else { ?>
+
+    <body>
+
+        <!-- main content -->
+        <div class="main-container">
+            <!-- left box: Sign in form -->
+            <div class="left-box">
+                <h1 id="login-header">User Sign In</h1>
+                <form id="login-form" action="login.php" method="POST">
+                    <?php
+                    if (isset($_SESSION['error'])) {
+                        echo '<p class="error">' . $_SESSION['error'] . '</p>';
+                        unset($_SESSION['error']);
+                    }
                     ?>
-                    <nav class="header-nav">
-                        <ul class="navigation-bar">
-                            <li><a href="homePage.php">Home</a></li>
-                            <li><a href="aboutUsPage.php">About Us</a></li>
-                            <li><a href="ourCatsPage.php">Our Cats</a></li>
-                            <nav class=CatCare>
-                                <a href="catCarePage.php"><button class="dropbtn">Cat Care</button></a>
-                                <nav class="products-content">
-                                    <a href="faqPage.php">FAQs</a>
-                                    <a href="catCarePage.php">Advice on Cat Care</a>
-                                    <a href="vetServicesPage.php">Vetinary Services</a>
-                                </nav>
-                                <nav class=AdminDropDown>
-                                    <a href="adminAccountPage.php"><button class="dropbtn">Your Admin Account</button></a>
-                                    <nav class="products-content">
-                                        <a href="adminEditAccountPage.php">Edit Your Account</a>
-                                        <a href="adminViewApplicationsPage.php">View Adoption Applications</a>
-                                        <a href="adminAppointmentsDatabasePage.php">View Appointments</a>
-                                        <a href="adminDonationsDatabasePage.php">Donations Database</a>
-                                        <a href="adminSponsorshipDatabasePage.php">Sponsorships Database</a>
-                                        <a href="adminStoriesDatabasePage.php">Success Stories Database</a>
-                                        <a href="adminUserDatabasePage.php">User Database</a>
-                                    </nav>
-                                </nav>
-                                <li><a href="contactUsPage.php">Contact Us </a></li>
-                                <button><a href="userMakeDonationPage.php">Donate</a></button>
-                                <button><a href="logout.php">Logout</a></button>
-                        </ul>
-                    </nav>
-                </nav>
-                <?php
-                } else if (isset($_SESSION['admin_status']) && $_SESSION['admin_status'] == 0) {
-                    ?>
-                    <nav class="header-nav">
-                        <ul class="navigation-bar">
-                            <li><a href="homePage.php">Home</a></li>
-                            <li><a href="aboutUsPage.php">About Us</a></li>
-                            <li><a href="ourCatsPage.php">Our Cats</a></li>
-                            <nav class=CatCare>
-                                <a href="catCarePage.php"><button class="dropbtn">Cat Care</button></a>
-                                <nav class="products-content">
-                                    <a href="faqPage.php">FAQs</a>
-                                    <a href="catCarePage.php">Advice on Cat Care</a>
-                                    <a href="vetServicesPage.php">Vetinary Services</a>
-                                </nav>
-                                <nav class=AdminDropDown>
-                                    <a href="userAccountPage.php"><button class="dropbtn">Your Account</button></a>
-                                    <nav class="products-content">
-                                        <a href="userEditAccountPage.php">Edit Your Account</a>
-                                        <a href="userViewApplicationsPage.php">View Adoption Applications</a>
-                                        <a href="userViewAppointmentsPage.php">View Appointments</a>
-                                        <a href="userDonationHistoryPage.php">Donations Database</a>
-                                        <a href="userViewSponsorshipsPage.php">Sponsorships Database</a>
-                                    </nav>
-                                </nav>
-                                <li><a href="contactUsPage.php">Contact Us </a></li>
-                                <button><a href="userMakeDonationPage.php">Donate</a></button>
-                                <button><a href="logout.php">Logout</a></button>
-                        </ul>
-                    </nav>
-                    </nav>
-                <?php
-                }
-            } else {
-                ?>
-            <nav class="header-nav">
-                <ul class="navigation-bar">
-                    <li><a href="homePage.php">Home</a></li>
-                    <li><a href="aboutUsPage.php">About Us</a></li>
-                    <li><a href="ourCatsPage.php">Our Cats</a></li>
-                    <nav class=CatCare>
-                        <a href="catCarePage.php"><button class="dropbtn">Cat Care</button></a>
-                        <nav class="products-content">
-                            <a href="faqPage.php">FAQs</a>
-                            <a href="catCarePage.php">Advice on Cat Care</a>
-                            <a href="vetServicesPage.php">Vetinary Services</a>
-                        </nav>
-                        <li><a href="contactUsPage.php">Contact Us </a></li>
-                        <button><a href="userMakeDonationPage.php">Donate</a></button>
-                        <button><a href="userSignInPage.php">Sign In</a></button>
-                </ul>
-            </nav>
-            </nav>
-            <?php
-            }
-            ?>
-        </nav>
+                    <input type="email" id="email" name="email" placeholder="Email" required><br><br>
+                    <input type="password" id="password" name="password" placeholder="Password" required><br><br>
+                    <a href="changePasswordPage.php">Forgotten Password?</a><br><br>
+                    <input type="submit" id="login" name="login" value="Login">
+                    <input type="button" value="Register" class="button" onclick="location.href='userSignUpPage.php';">
+                    <input type="button" value="Admin Login" class="button" onclick="location.href='adminSignInPage.php';">
+                </form>
+            </div>
 
-    <!-- page title -->
-    <div class="page-title">User Sign In</div>
-
-    <!-- create account link -->
-    <div class="create-account-box">
-        <a href="userSignUpPage.php">Create an Account</a>
-    </div>
-
-    <!-- main content -->
-    <div class="main-container">
-        <!-- left box: Sign in form -->
-        <div class="left-box">
-            <h1 id="login-header">User Sign In</h1>
-            <form id="login-form" action="login.php" method="POST">
-                <?php
-                if (isset($_SESSION['error'])) {
-                    echo '<p class="error">' . $_SESSION['error'] . '</p>';
-                    unset($_SESSION['error']);
-                }
-                ?>
-                <input type="email" id="email" name="email" placeholder="Email" required><br><br>
-                <input type="password" id="password" name="password" placeholder="Password" required><br><br>
-                <a href="changePasswordPage.php">Forgotten Password?</a><br><br>
-                <input type="submit" id="login" name="login" value="Login">
-                <input type="button" value="Register" class="button"
-                    onclick="location.href='userSignUpPage.php';">
-                <input type="button" value="Admin Login" class="button"
-                    onclick="location.href='adminSignInPage.php';">
-            </form>
+            <!-- right box: image -->
+            <div class="right-box">
+                <img src="../nlhImages/cat1.jpg" alt="Cat Image">
+            </div>
         </div>
-
-        <!-- right box: image -->
-        <div class="right-box">
-            <img src="placeholder-image.jpg" alt="Cat Image">
-        </div>
-    </div>
-</body>
-
-        <!-- footer -->
-<footer class="footer">
-    <div class="footer-section">
-        <div>
-            <img src="logo.png" class="logo" alt="Company Logo"></a>
-        </div>
-        <div>
-
-        <p>
-                Images and products on this page were sourced from the following websites:<br>
-                <br>
-                https://www.istockphoto.com/search/2/image-film?phrase=cat+rescue <br>
-
-                <br>
-                © 2024 NineLivesHaven. All rights reserved.
-
-                The content, design, and images on this website are the property of Nine Lives Haven and are protected
-                by
-                international copyright laws. Unauthorized use or reproduction of any materials without the express
-                written
-                consent of Nine Lives Haven is strictly prohibited. Nine Lives Haven and the Nine Lives Haven logo are
-                trademarks of
-                Nine Lives Haven.
-
-                For inquiries regarding the use or reproduction of any content on this website, please contact us at
-                nineliveshaven@rescue.com
-            </p>
-
-        </div>
-    </div>
-</footer>
+    </body>
+<?php }
+include 'footer.php'; // footer ?>
 
 </html>
-<?php } ?>
