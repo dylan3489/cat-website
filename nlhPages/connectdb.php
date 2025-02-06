47a9727077b2f77a0b21103d1b0e742b1c85b88c
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2025-02-06 11:54:54 +0000

    Update connectdb.php
    commented out connection message

--- nlhPages/connectdb.php
+++ nlhPages/connectdb.php
@@ -7,11 +7,11 @@ $dbName = "ninelives";
 
 $con = mysqli_connect($serverName, $userName, $password, $dbName);
 
-if (mysqli_connect_errno()) {
+/*if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
 } else {
     echo "Database connection successful!";
-}
+}*/
 ?>
 
