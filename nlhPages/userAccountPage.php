fecda8c49dd12fd8ece913689190a5c333f381ca
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2025-02-20 13:22:51 +0000

    Update userAccountPage.php
    updated CSS and layout of page

--- nlhPages/userAccountPage.php
+++ nlhPages/userAccountPage.php
@@ -7,12 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
+    <link rel="stylesheet" href="../nlhCSS/userAccountCSS.css">
 </head>
 
 <?php
 
 session_start();
 require 'connectdb.php';
+include 'navbar.php'; // banner and nav bar
+
 
 // if the user is not logged in, redirect to sign in page
 if(!isset($_SESSION['user_id'])){
@@ -28,106 +31,8 @@ $user = mysqli_fetch_assoc($result);
 ?>
 
 <body>
-    <header>
-        <nav class="banner">
-            <a href="homePage.php"><img src="logo" class="logo" alt="Company Logo"></a>
-            <!-- this is the navigation bar -written using php to show 1 of three versions depending on 
-                    the type of user i.e. Admin, Registered User or Visitor-->
-            <?php
-            if (isset($_SESSION['loggedin'])) {
-                if (isset($_SESSION['admin_status']) && $_SESSION['admin_status'] == 1) {
-                    ?>
-                    <nav class="header-nav">
-                        <ul class="navigation-bar">
-                            <li><a href="homePage.php">Home</a></li>
-                            <li><a href="aboutUsPage.php">About Us</a></li>
-                            <li><a href="ourCatsPage.php">Our Cats</a></li>
-                            <nav class=CatCare>
-                                <a href="catCarePage.php"><button class="dropbtn">Cat Care</button></a>
-                                <nav class="products-content">
-                                    <a href="faqPage.php">FAQs</a>
-                                    <a href="catCarePage.php">Advice on Cat Care</a>
-                                    <a href="vetServicesPage.php">Vetinary Services</a>
-                                </nav>
-                                <nav class=AdminDropDown>
-                                    <a href="adminAccountPage.php"><button class="dropbtn">Your Admin Account</button></a>
-                                    <nav class="products-content">
-                                        <a href="adminEditAccountPage.php">Edit Your Account</a>
-                                        <a href="adminViewApplicationsPage.php">View Adoption Applications</a>
-                                        <a href="adminAppointmentsDatabasePage.php">View Appointments</a>
-                                        <a href="adminDonationsDatabasePage.php">Donations Database</a>
-                                        <a href="adminSponsorshipDatabasePage.php">Sponsorships Database</a>
-                                        <a href="adminStoriesDatabasePage.php">Success Stories Database</a>
-                                        <a href="adminUserDatabasePage.php">User Database</a>
-                                    </nav>
-                                </nav>
-                                <li><a href="contactUsPage.php">Contact Us </a></li>
-                                <button><a href="userMakeDonationPage.php">Donate</a></button>
-                                <button><a href="logout.php">Logout</a></button>
-                        </ul>
-                    </nav>
-                </nav>
-                <?php
-                } else if (isset($_SESSION['admin_status']) && $_SESSION['admin_status'] == 0) {
-                    ?>
-                    <nav class="header-nav">
-                        <ul class="navigation-bar">
-                            <li><a href="homePage.php">Home</a></li>
-                            <li><a href="aboutUsPage.php">About Us</a></li>
-                            <li><a href="ourCatsPage.php">Our Cats</a></li>
-                            <nav class=CatCare>
-                                <a href="catCarePage.php"><button class="dropbtn">Cat Care</button></a>
-                                <nav class="products-content">
-                                    <a href="faqPage.php">FAQs</a>
-                                    <a href="catCarePage.php">Advice on Cat Care</a>
-                                    <a href="vetServicesPage.php">Vetinary Services</a>
-                                </nav>
-                                <nav class=AdminDropDown>
-                                    <a href="userAccountPage.php"><button class="dropbtn">Your Account</button></a>
-                                    <nav class="products-content">
-                                        <a href="userEditAccountPage.php">Edit Your Account</a>
-                                        <a href="userViewApplicationsPage.php">View Adoption Applications</a>
-                                        <a href="userViewAppointmentsPage.php">View Appointments</a>
-                                        <a href="userDonationHistoryPage.php">Donations Database</a>
-                                        <a href="userViewSponsorshipsPage.php">Sponsorships Database</a>
-                                    </nav>
-                                </nav>
-                                <li><a href="contactUsPage.php">Contact Us </a></li>
-                                <button><a href="userMakeDonationPage.php">Donate</a></button>
-                                <button><a href="logout.php">Logout</a></button>
-                        </ul>
-                    </nav>
-                    </nav>
-                <?php
-                }
-            } else {
-                ?>
-            <nav class="header-nav">
-                <ul class="navigation-bar">
-                    <li><a href="homePage.php">Home</a></li>
-                    <li><a href="aboutUsPage.php">About Us</a></li>
-                    <li><a href="ourCatsPage.php">Our Cats</a></li>
-                    <nav class=CatCare>
-                        <a href="catCarePage.php"><button class="dropbtn">Cat Care</button></a>
-                        <nav class="products-content">
-                            <a href="faqPage.php">FAQs</a>
-                            <a href="catCarePage.php">Advice on Cat Care</a>
-                            <a href="vetServicesPage.php">Vetinary Services</a>
-                        </nav>
-                        <li><a href="contactUsPage.php">Contact Us </a></li>
-                        <button><a href="userMakeDonationPage.php">Donate</a></button>
-                        <button><a href="userSignInPage.php">Sign In</a></button>
-                </ul>
-            </nav>
-            </nav>
-            <?php
-            }
-            ?>
-        </nav>
-    </header>
-
     <section class="page-title-section">
-        <h1>User Account Overview</h1>
+        <h1>Your Account</h1>
     </section>
 
     <!-- user account details with two boxes -->
@@ -139,16 +44,16 @@ $user = mysqli_fetch_assoc($result);
             <p><strong>Phone Number:</strong> <?php echo $user['phone_number']; ?></p>
             <p><strong>Address:</strong>
                 <?php echo $user['street_address'] . ', ' . $user['city'] . ', ' . $user['post_code']; ?></p>
-            <a href="userEditAccountPage.php" class="edit-details-btn">Edit Details</a>
+            <a href="userEditDetails.php" class="edit-details-btn">Edit Details</a>
         </div>
 
         <div class="account-box-right">
             <h2>Quick Links</h2>
             <ul>
-                <li><a href="userViewApplicationsPage.php">View Adoption Applications</a></li>
-                <li><a href="userViewAppointmentsPage.php">View Appointments</a></li>
-                <li><a href="userDonationHistoryPage.php">Donation History</a></li>
-                <li><a href="userViewSponsorshipsPage.php">View Sponsorships</a></li>
+                <li><a href="userViewApplicationsPage.php">Your Adoption Applications</a></li>
+                <li><a href="userViewAppointmentsPage.php">Your Appointments</a></li>
+                <li><a href="userDonationHistoryPage.php">Your Donation History</a></li>
+                <li><a href="userViewSponsorshipsPage.php">Your Sponsorships</a></li>
             </ul>
             <a href="userBookAppointmentPage.php" class="book-appointment-btn">Book an Appointment</a>
         </div>
@@ -156,35 +61,7 @@ $user = mysqli_fetch_assoc($result);
 
 </body>
 
-<footer class="footer">
-    <div class="footer-section">
-        <div>
-            <img src="logo.png" class="logo" alt="Company Logo"></a>
-        </div>
-        <div>
-
-            <p>
-                Images and products on this page were sourced from the following websites:<br>
-                <br>
-                https://www.istockphoto.com/search/2/image-film?phrase=cat+rescue <br>
-
-                <br>
-                © 2024 NineLivesHaven. All rights reserved.
-
-                The content, design, and images on this website are the property of Nine Lives Haven and are protected
-                by
-                international copyright laws. Unauthorized use or reproduction of any materials without the express
-                written
-                consent of Nine Lives Haven is strictly prohibited. Nine Lives Haven and the Nine Lives Haven logo are
-                trademarks of
-                Nine Lives Haven.
-
-                For inquiries regarding the use or reproduction of any content on this website, please contact us at
-                nineliveshaven@rescue.com
-            </p>
-
-        </div>
-    </div>
-</footer>
+<?php include 'footer.php'; // footer
+?>
 
 </html>
