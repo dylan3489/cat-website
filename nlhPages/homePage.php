0592b4e613d23d52f4f8c4d55c32b465adc9ecbf
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2025-04-26 23:09:49 +0100

    Update homePage.php
    finished page

--- nlhPages/homePage.php
+++ nlhPages/homePage.php
@@ -23,7 +23,11 @@ include 'navbar.php'; // banner and nav bar
         <img src="../nlhImages/background.jpg" alt="Background Image">
         <div class="intro-text">
             <h1>Welcome to Nine Lives Haven</h1>
-            <p>Find out more about our feline friends and how you can support them and start your adoption journey!
+            <p>At Nine Lives Haven, it is our mission to make sure we can take in and help as many cats possible to find
+                a place they can call home, and to provide as high a level of support a we can to new adopters that
+                are bravely tackling the responsibilities of taking in one of our little felines. <br> <br>
+
+                Find out more about our feline friends and how you can support them and start your adoption journey!
                 <a href="ourCatsPage.php"><br><br>See our cats!</a>
             </p>
         </div>
@@ -56,9 +60,15 @@ include 'navbar.php'; // banner and nav bar
 
 
     <section class="cat-care-section">
-        <h2>Getting Overwhelmed? Visit our Cat Care section!</h2>
-        <p>Don't worry! We have a bunch of easily accessible information on all you need to know about
-            cat rescue, the adoption process, what you need to consider and more! <br>
+        <h2>Considering adoption, but feeling unsure about the responsiblity? Visit our Cat Care section!</h2>
+        <p>Don't worry! We understand that taking in a cat can be a daunting concept, especially one that could come
+            from a troubled background.
+            <br> <br>
+            We want to ensure that they are looked after to the best of our ability, and this means helping out their
+            new homeowners with the responsibilities and important details that come with adoption!
+
+            <br> <br> Our cat care sections provides some helpful, accessible information on looking after a cat, the
+            adoption process, links to further resources and more. <br> <br>
             <a href="catCarePage.php"> Click here!</a>
 
         </p>
